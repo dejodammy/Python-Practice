cc832bb14a2edad506b1468399227d90c042d64b
dejodammy <95016055+dejodammy@users.noreply.github.com>
2025-02-23 01:50:20 +0100

    commit

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -6,13 +6,16 @@ FONT = ("coriel", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.highscore = 0
+        self.highscore_file = "highscore.txt"
+        with open(self.highscore_file, "r") as f:
+            self.highscore = int(f.read())
         self.color("White")
         self.penup()
         self.goto(0,270)
         self.score = 0
         self.hideturtle()
         self.update_score()
+        
         
         
     def update_score(self):
@@ -22,6 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+        with open(self.highscore_file, "w") as f:
+            f.write(str(self.highscore))
         self.score = 0
         self.update_score()
 
